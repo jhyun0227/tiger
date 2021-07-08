ba0d8dc6f06f860ccefda6b8030f31d88c879a91
jhyun0227 <jhyun0227@naver.com>
2021-07-08 23:04:34 +0900

    0708 학원에서 해라

--- src/main/java/com/ch/tiger/controller/MemberController.java
+++ src/main/java/com/ch/tiger/controller/MemberController.java
@@ -16,19 +16,19 @@ public class MemberController {
 	private MemberService mbs;
 	
 	@RequestMapping("loginForm")
-	public String loginForm() {
+	public String loginForm() { // 로그인 폼으로 이동
 		return "login/loginForm";
 	}
 	
 	@RequestMapping("login")
-	public String login(Member member, Model model, HttpSession session) {
+	public String login(Member member, Model model, HttpSession session) { // 로그인
 		int result = 0;
 		Member member2 = mbs.select(member.getMB_id());
-		if (member2 == null || member2.getMB_del().equals("y")) {
+		if (member2 == null || member2.getMB_del().equals("y")) { // 계정이 없거나 del컬럼이 y인 경우
 			result = -1; // 없는 아이디
-		} else if (member2.getMB_pw().equals(member.getMB_pw())) {
+		} else if (member2.getMB_pw().equals(member.getMB_pw())) { // 아이디와 비밀번호가 일치할 경우 1 비밀번호가 틀리면 0
 			result = 1; // 성공 id와 password 일치
-			session.setAttribute("id", member.getmeId());
+			session.setAttribute("id", member.getMB_id());
 		}
 		model.addAttribute("result", result);
 		return "login/memberMain";

f920826b6927538c5ef0df5eaa89a2c93d46d169
admin <admin@DESKTOP-OLVM896>
2021-07-08 20:06:46 +0900

    0708 집가서해라

--- src/main/java/com/ch/tiger/controller/MemberController.java
+++ src/main/java/com/ch/tiger/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.ch.tiger.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ch.tiger.model.Member;
 import com.ch.tiger.service.MemberService;
 
 @Controller
@@ -14,5 +18,19 @@ public class MemberController {
 	@RequestMapping("loginForm")
 	public String loginForm() {
 		return "login/loginForm";
+	}
+	
+	@RequestMapping("login")
+	public String login(Member member, Model model, HttpSession session) {
+		int result = 0;
+		Member member2 = mbs.select(member.getMB_id());
+		if (member2 == null || member2.getMB_del().equals("y")) {
+			result = -1; // 없는 아이디
+		} else if (member2.getMB_pw().equals(member.getMB_pw())) {
+			result = 1; // 성공 id와 password 일치
+			session.setAttribute("id", member.getmeId());
+		}
+		model.addAttribute("result", result);
+		return "login/memberMain";
 	}
 }
--- src/main/webapp/WEB-INF/views/login/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/login/loginForm.jsp
@@ -7,7 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-<form action="login">
+<form action="login.do">
 	<table class="table table-bordered">
 		<tr>
 			<td>아이디</td>
